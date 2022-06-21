aa59f25cb002af7605dcd73d44b8232d9956ab2e
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-06-21 00:08:49 -0400

    desktop/website merge

--- resources/page.js
+++ resources/page.js
@@ -1,9 +1,9 @@
-var cmd=document.body.children["cmd"]
-app={
-    terminal:cmd.children["display"],
-    cmd:cmd.children["display"].children[0].children[0],
-    buffer:120}
-GUI.compile(["Personalize Steam app initalized. No license reserved.",""])
+
+window.onload=function(){
+    if(navigator.userAgent.includes("Electron")){
+        var p=document.createElement("style")
+        p.appendChild(document.createTextNode(".desktop{display:flex}"))
+        document.head.appendChild(p)}}
 CMD.set("cls","GUI.clear(document.body.children['cmd']);")
 var API={
     menu:function(e){
@@ -13,3 +13,12 @@ var API={
         }else{
             e.children[1].style.width="15px"
             e.children[2].style.width="0px"}}}
+            
+//object indexing
+var taskbar=document.body.children["titlebar"]
+var cmd=document.body.children["cmd"]
+var app={
+    terminal:cmd.children["display"],
+    cmd:cmd.children["display"].children[0].children[0],
+    buffer:120}
+GUI.compile(["Personalize Steam app initalized. No license reserved.",""])
