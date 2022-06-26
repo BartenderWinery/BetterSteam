b56979982a87862d9a0813f5099ca580259488bd
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-06-26 02:18:30 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,25 +1,15 @@
 const {
     contextBridge,
     ipcRenderer
-} = require("electron");
+} = require("electron")
 
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld(
     "api", {
         send: (channel, data) => {
-            // whitelist channels
             let validChannels = "minimize;maximize;close;popout;exists";
             if (validChannels.includes(channel)) {
-                ipcRenderer.send(channel, data);
-            }
-        },
+                ipcRenderer.send(channel, data)}},
         receive: (channel, func) => {
             let validChannels = "minimize;maximize;close;popout;exists";
             if (validChannels.includes(channel)) {
-                // Deliberately strip event as it includes `sender` 
-                ipcRenderer.on(channel, (event, ...args) => func(...args));
-            }
-        }
-    }
-);
+                ipcRenderer.on(channel, (event, ...args) => func(...args))}}})
