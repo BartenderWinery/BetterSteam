22f3934385cb07b439dfa3051f093b3c8dc643f4
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-06-28 02:56:05 -0400

    you can uninstall too now, though its a bit lazy; I need to convert it to polymorph

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ var rcon={
                 if(err)return
                 //Change comments to start/end comments per line to reserve comments instead of removing them
                 var pack=data.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'').replace(/[\r\n]+/g,"")
-                    //debug indented
                 fs.writeFile(path,pack.replace(k,d),(err)=>{
                     if(err)return})})
             return true
@@ -39,15 +38,18 @@ ipcMain.on("minimize",()=>{BrowserWindow.getFocusedWindow().minimize()})
 ipcMain.on("maximize",()=>{BrowserWindow.getFocusedWindow().maximize()})
 ipcMain.on("close",()=>{BrowserWindow.getFocusedWindow().close()})
 ipcMain.on("popout",()=>{popout([400,220],"resources/install.html")})
+
+//CONVERT INTO POLYTHESIM OR FUNCTION FOR PERFORMANCE
+    //create callback function to update the other if one is executed, such as uninstaller decolorizing APIs dyanmically if install is open
 ipcMain.on("build",(events,args)=>{
     fs.access(args,fs.constants.R_OK|fs.constants.W_OK,(err,data)=>{
-        if(!err){
+        if(err){BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log(\"Build dictionary wasn't found, please check the path.\");document.activeElement.value=''");return}
             BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Build dictionary verified.');document.activeElement.parentElement.innerText='"+args+"';document.activeElement.value=''")
             if(rcon.inject(args+"/steamui/index.html","</body>","<div id=\"debug\" style=\"position:absolute;height:100%;width:400px;right:0;top:0\"></div><script>function traceMethodCalls(obj){;return new Proxy(obj, {get(target, methodName, receiver) {const originMethod = target[methodName];return function(...args) {document.body.children[\"debug\"].insertAdjacentHTML(\"afterbegin\",\"<p style='font-size:10px;margin:0;text-align:right;color:red;white-space:nowrap'>\"+args+\"</p>\");return originMethod.apply(this, args);}}})}console = traceMethodCalls(console)</script></body>"))
-                BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Steamui modified;Dev-only currently');document.body.children[1].children[1].children[1].children[1].style.color='#00eb00'")
-            //rcon.inject("D:/Steam/resource/menus/steam.menu")
-            //BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Steam modified; Appended script');document.body.children[1].children[1].children[1].children[0].style.color='#00eb00'")
-            
-            //BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Tenfoot modified; Injected script');document.body.children[1].children[1].children[1].children[7].style.color='#00eb00'")
-            return}
-        BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log(\"Build dictionary wasn't found, please check the path.\");document.activeElement.value=''")})})
+                BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Steamui modified;Dev-only currently');document.body.children[1].children[1].children[1].children[1].style.color='#00eb00'")})})
+ipcMain.on("uninstall",(events,args)=>{
+    fs.access(args,fs.constants.R_OK|fs.constants.W_OK,(err,data)=>{
+        if(err){BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log(\"Built dictionary wasn't found, please check the path.\");document.activeElement.value=''");return}
+            BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Built dictionary verified.');document.activeElement.parentElement.innerText='"+args+"';document.activeElement.value=''")
+            if(rcon.inject(args+"/steamui/index.html","<div id=\"debug\" style=\"position:absolute;height:100%;width:400px;right:0;top:0\"></div><script>function traceMethodCalls(obj){;return new Proxy(obj, {get(target, methodName, receiver) {const originMethod = target[methodName];return function(...args) {document.body.children[\"debug\"].insertAdjacentHTML(\"afterbegin\",\"<p style='font-size:10px;margin:0;text-align:right;color:red;white-space:nowrap'>\"+args+\"</p>\");return originMethod.apply(this, args);}}})}console = traceMethodCalls(console)</script></body>","</body>"))
+                BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Steamui recovered');document.body.children[1].children[1].children[1].children[1].style.color='red'")})})
--- preload.js
+++ preload.js
@@ -6,10 +6,10 @@ const {
 contextBridge.exposeInMainWorld(
     "api", {
         send: (channel, data) => {
-            let validChannels = "minimize;maximize;close;popout;build";
+            let validChannels = "minimize;maximize;close;popout;build;uninstall";
             if (validChannels.includes(channel)) {
                 ipcRenderer.send(channel, data)}},
         receive: (channel, func) => {
-            let validChannels = "minimize;maximize;close;popout;build";
+            let validChannels = "minimize;maximize;close;popout;build;uninstall";
             if (validChannels.includes(channel)) {
                 ipcRenderer.on(channel, (event, ...args) => func(...args))}}})
