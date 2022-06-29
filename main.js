f8939edb5579606bead35757bbec041879ea5102
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-06-29 14:28:47 -0400

    not so secretive secret added: dog

--- main.js
+++ main.js
@@ -5,21 +5,21 @@ const fs=require("fs")
 require("electron-reload")(__dirname, {
     electron:require(`${__dirname}/node_modules/electron`)})
 
-var win=[]
-function popout(bounds,page){
-    win[win.length]=new BrowserWindow({
+function popout(bounds,page,parent){
+    var win=new BrowserWindow({
         width:bounds[0],
         height:bounds[1],
         frame:false,
         show:false,
         backgroundColor:"#383838",
+        parent: parent,
         webPreferences:{
             nodeIntegration:false,
             contextIsolation:true,
             enableRemoteModule:false,
             preload:path.join(__dirname,"preload.js")}})
-        win[win.length-1].loadFile(path.join(__dirname,page))
-        setTimeout(function(){win[win.length-1].show()}),500}
+        win.loadFile(path.join(__dirname,page))
+        setTimeout(function(){win.show()}),500}
 async function main(){popout([580,300],"index.html")}
 app.on("ready",main)
 var rcon={
@@ -46,14 +46,16 @@ ipcMain.on("popout",(events,args)=>{popout([400,220],args)})
 ipcMain.on("modify",(events,args)=>{
     switch(args[0]){
         case "uninstall":
-            rcon.overwrite(args[1]+"/steamui/index.html","<!doctype html><html style=\"width: 100%; height: 100%\"><head><title>Steam</title><meta charset=\"utf-8\"><link href=\"/css/library.css\" rel=\"stylesheet\"></head><body style=\"width: 100%; height: 100%; margin: 0; overflow: hidden;\"><div id=\"root\" style=\"height:100%; width: 100%\"></div><div style=\"display:none\"></div><script src=\"/libraries/libraries.js\"></script><script src=\"/libraries/libraries_cm.js\"></script><script src=\"/library.js\"></script></body></html>");return
+            if(rcon.overwrite(args[1]+"/steamui/index.html","<!doctype html><html style=\"width: 100%; height: 100%\"><head><title>Steam</title><meta charset=\"utf-8\"><link href=\"/css/library.css\" rel=\"stylesheet\"></head><body style=\"width: 100%; height: 100%; margin: 0; overflow: hidden;\"><div id=\"root\" style=\"height:100%; width: 100%\"></div><div style=\"display:none\"></div><script src=\"/libraries/libraries.js\"></script><script src=\"/libraries/libraries_cm.js\"></script><script src=\"/library.js\"></script></body></html>")){BrowserWindow.getAllWindows().at(-1).webContents.executeJavaScript("SYS.compile(['Steam modifications uninstalled; Files recovered'])");return}
+            BrowserWindow.getAllWindows().at(-1).webContents.executeJavaScript("SYS.compile(['Could not recover files in specified location, Please check spelling.'])")
         case "install":
             if(args[1]=="undefined"){popout([400,220],"resources/install.html");return}
             fs.access(args[1],fs.constants.R_OK|fs.constants.W_OK,(err,data)=>{
             if(err){BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log(\"Build dictionary wasn't found, please check the path.\");document.activeElement.value=''");return}
                 BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Build dictionary verified.');document.activeElement.parentElement.innerText='"+args[1]+"';document.activeElement.value=''")
                 if(rcon.inject(args[1]+"/steamui/index.html","</body>","<div id=\"debug\" style=\"position:absolute;height:100%;width:400px;right:0;top:0\"></div><script>function traceMethodCalls(obj){;return new Proxy(obj, {get(target, methodName, receiver) {const originMethod = target[methodName];return function(...args) {document.body.children[\"debug\"].insertAdjacentHTML(\"afterbegin\",\"<p style='font-size:10px;margin:0;text-align:right;color:red;white-space:nowrap'>\"+args+\"</p>\");return originMethod.apply(this, args);}}})}console = traceMethodCalls(console)</script></body>"))
-                    BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Steamui modified;Dev-only currently');document.body.children[1].children[1].children[1].children[1].style.color='#00eb00'")})
+                    BrowserWindow.getFocusedWindow().webContents.executeJavaScript("log('Steamui modified;Dev-only currently');document.body.children[1].children[1].children[1].children[1].style.color='#00eb00'")
+            BrowserWindow.getAllWindows().at(-1).webContents.executeJavaScript("SYS.compile(['Steam has been successfully modified'])")})
         case "mod":
             }})
 
