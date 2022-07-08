bd830bf8088b2fdce37f04a04e30885b4e49d810
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-07-08 04:53:31 -0400

    MOD SUPPORT!!!

--- main.js
+++ main.js
@@ -23,30 +23,12 @@ function popout(bounds,page){
         setTimeout(function(){win.show()}),500}
 async function main(){
     popout([580,300],"index.html")
-    fs.readFile(__dirname+"/"+"config/app.txt",'utf8',(e,data)=>{
-        if(e)return
-        if(!String(data).includes("active")){
+    fs.readFile(__dirname+"/config/app.txt",(e,data)=>{
+        if(e||!String(data).includes("active")){
             BrowserWindow.getAllWindows()[0].webContents.executeJavaScript("dis()")
-            fs.appendFile(__dirname+"/"+"config/app.txt",";active",(e)=>{if(e)return})}})}
+            fs.appendFile(__dirname+"/config/app.txt",";active",(e)=>{if(e)return})}})}
 app.on("ready",main)
 var sys={
-    compile:function(){
-        fs.readdir(__dirname+"/cache",function(e,lib){
-            if(e)return
-            for(var i=0;i<lib.length;i++)
-                fs.unlink(__dirname+"/cache/"+lib[i],(e)=>{
-                    if(e)return})})
-        fs.readdir(__dirname+"/mods",function(e,lib){
-            if(e)return
-            for(var i=0;i<lib.length;i++)
-                fs.readFile(__dirname+"/mods/"+lib[i],(e,d)=>{
-                    if(e)return
-                    var d=JSON.parse(d)
-                    var dat=Object.keys(d)
-                    for(var _i=0;_i<dat.length;_i++)
-                        for(var __i=0;__i<d[dat[_i]].length;__i++)
-                            fs.appendFile(__dirname+"/cache/"+dat[_i]+".js",d[dat[_i]][__i],(e)=>{
-                                if(e)return})})})},
     overwrite:function(path,d){
         try{
             fs.writeFile(path,d,(e)=>{
@@ -97,18 +79,19 @@ ipcMain.on("modify",(events,args)=>{
                 wins.at(-1).webContents.executeJavaScript("SYS.compile(['Build dictionary verified.',''])")
                 wins[0].webContents.executeJavaScript("log(\"Build dictionary verified.\")")
                 fs.readdir(__dirname+"/resources/libraries",function(e,lib){
+                    var j,file
                     if(e){
                         wins.at(-1).webContents.executeJavaScript("SYS.compile(['Library failure, please vaildate your files.'])")
                         return}
-                    for(var i=0;i<lib.length;i++)
+                    for(var i=0;i<lib.length;i++){
                         file=lib[i]
                         fs.readFile(__dirname+"/resources/libraries/"+file,'utf8',(e,data)=>{
                             if(e){
                                 wins.at(-1).webContents.executeJavaScript("SYS.compile(['Failure, please check permissions.'])")
                                 return}
-                            var j=JSON.parse(data)
+                            j=JSON.parse(data)
                             var ids=j["success"]["ids"].split(";")
-                            if(sys.overwrite(args[1]+j["path"],j[args[0]].replace("{PATH}",__dirname+"/cache/global.js").replace("{MODPATH}",__dirname+"/cache/"+file.split(".")[0]+".js"))){
+                            if(sys.overwrite(args[1]+j["path"],j[args[0]].replace("{PATH}","/"+file.split(".")[0]+".js"))){
                                 switch(args[0]){
                                     case "uninstall":
                                         fs.writeFile(__dirname+"/config/path.txt","",(e)=>{
@@ -126,6 +109,42 @@ ipcMain.on("modify",(events,args)=>{
                                         wins.at(-1).webContents.executeJavaScript("SYS.compile(['"+j["success"]["log"]+"'])")
                                         for(var i=0;i<ids.length;i++)
                                             if(wins[0])wins[0].webContents.executeJavaScript("document.body.children[1].children["+ids[i][0]+"].children[1].children["+ids[i][2]+"].style.color='#00eb00'")
-                                        sys.compile()
                                         break}
-                                return}})})})}})
+                                fs.readdir(__dirname+"/mods",function(e,pack){
+                                    if(e)return
+                                    for(var i=0;i<pack.length;i++){
+                                        var mod=pack[i]
+                                        var json,dat
+                                        fs.readFile(__dirname+"/mods/"+mod,(e,d)=>{
+                                            if(e)return
+                                            json=JSON.parse(d)
+                                            dat=Object.keys(json)
+                                            for(var _i=0;_i<dat.length;_i++){
+                                                var p=args[1]+j["path"].replace(j["path"].split("/").at(-1),file.split(".")[0]+".js")
+                                                switch(args[0]){
+                                                    case "install":
+                                                        fs.appendFile(p,json[dat[_i]],(e)=>{
+                                                            if(e)return})
+                                                        return
+                                                    case "uninstall":
+                                                        fs.unlink(p,(e)=>{
+                                                            if(e)return})
+                                                        return}}})}})}})}})})}})
+                                                        
+                                                            //compile:function(path){
+    //    fs.readdir(path+"/cache",function(e,lib){
+    //        if(e)return
+    //        for(var i=0;i<lib.length;i++)
+    //            fs.unlink(path+"/cache/"+lib[i],(e)=>{
+    //                if(e)return})})
+    //    fs.readdir(__dirname+"/mods",function(e,lib){
+    //        if(e)return
+    //        for(var i=0;i<lib.length;i++)
+    //            fs.readFile(__dirname+"/mods/"+lib[i],(e,d)=>{
+    //                if(e)return
+    //                var d=JSON.parse(d)
+    //                var dat=Object.keys(d)
+    //                for(var _i=0;_i<dat.length;_i++)
+    //                    for(var __i=0;__i<d[dat[_i]].length;__i++)
+    //                        fs.appendFile(path+"/cache/"+dat[_i]+".js",d[dat[_i]][__i],(e)=>{
+    //                            if(e)return})})})},
